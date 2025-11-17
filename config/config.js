6ae376a425ec4691bdfbbef1f83e5c953347cd80
yatish-kbcd <yatishsharma@kbcdindia.com>
2025-11-17 17:35:09 +0530

    change get Lead create api url

--- config/config.js
+++ config/config.js
@@ -4,7 +4,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const config = {
-  getLeadCreate: process.env.getLeadCreate || 'https://pujariwala.in/jwt_vir_apis/get_lead_create',
+  getLeadCreate: process.env.getLeadCreate || 'https://pujariwala.in/jwt_vir_apis/get_googlesheet_lead_create',
 };
 
 export default config;
